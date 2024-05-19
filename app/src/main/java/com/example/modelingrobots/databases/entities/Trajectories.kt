83616752aba6b40526a2151803ffa32de7cd5bde
KIBinmir <104739414+KIBinmir@users.noreply.github.com>
2024-05-20 00:05:53 +0300

    Deleted not necessary files nad rewrited entity Trajectories

--- app/src/main/java/com/example/modelingrobots/databases/entities/Trajectories.kt
+++ app/src/main/java/com/example/modelingrobots/databases/entities/Trajectories.kt
@@ -12,5 +12,13 @@ data class Trajectories(
     @ColumnInfo(name = "type_moving") var typeMoving: String,
     @ColumnInfo(name = "type_coordinates") var typeCoordinates: Boolean, //true - relate, false - absolute
     @ColumnInfo(name = "velocity") var velocity: Double?,
-    @ColumnInfo(name = "list_point_moving") var listPointMoving: List<List<Double>>
+    @ColumnInfo(name = "point0") var point0: String,
+    @ColumnInfo(name = "point1") var point1: String,
+    @ColumnInfo(name = "point2") var point2: String,
+    @ColumnInfo(name = "point3") var point3: String,
+    @ColumnInfo(name = "point4") var point4: String,
+    @ColumnInfo(name = "point5") var point5: String,
+    @ColumnInfo(name = "point6") var point6: String,
+    @ColumnInfo(name = "point7") var point7: String,
+    @ColumnInfo(name = "point8") var point8: String
 )
